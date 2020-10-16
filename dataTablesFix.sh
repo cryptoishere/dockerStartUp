9c83013284ed98ae764dd851aac60dc3ebd1024a
cryptoishere <crypoishere88@gmail.com>
2020-10-16 22:04:14 +0300

    use config file to add mysql server configuration

--- dataTablesFix.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-echo "############   START DATATABLE FIX   ############"
-cd ../../../var/lib/mysql
-mysql -uroot -p${ROOT_PASSWORD} motherlink < ../../../docker-entrypoint-initdb.d/fixDump.sql
-echo "INJECT SQL into DB >>> SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));"
-echo "############   DATATABLE IS FIXED   ############"
